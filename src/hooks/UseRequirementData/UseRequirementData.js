8ec7f966a71129ea5e60bb31a7f5ef55ecef308b
rqualls <rachellqualls@gmail.com>
2023-11-21 23:30:54 -0600

    merge api.js updates for get and post in useRequirementData

--- src/hooks/UseRequirementData/UseRequirementData.js
+++ src/hooks/UseRequirementData/UseRequirementData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import InspectMocks from '../../views/Inspect/_test_/mocks';
-import axios from 'axios';
+// import axios from 'axios';
 import jsonData from '../../config.json';
 import PropTypes from 'prop-types';
 
@@ -17,11 +17,11 @@ const UseRequirementData = (files) => {
 
     const updateRequirementList = (newRequirements, updateIndex) => {
         const newRequirementList = requirementsList.map((requirements, index) => {
-        if (index == updateIndex) {
-            return newRequirements;
-        } else {
-            return requirements;
-        }
+            if (index == updateIndex) {
+                return newRequirements;
+            } else {
+                return requirements;
+            }
         })
         setrequirementsList(newRequirementList);
     }
@@ -45,67 +45,121 @@ const UseRequirementData = (files) => {
     };
     
     // post given file
-    const postFile = async (file, index) => {
-        try {
-            const base64Content = await fileToBase64(file);
-            const response = await axios.post(createURL(), {
+    // const postFile = async (file, index) => {
+    //     try {
+    //         const base64Content = await fileToBase64(file);
+    //         const response = await axios.post(createURL(), {
+    //             headers: {
+    //                 Accept: 'application/json'
+    //             },
+    //             body: base64Content
+    //         })
+    //         if (response.status == 200) {
+    //             requestArray[index] = {uuid: response.data.uuid, state: 'in progress'}
+    //         }
+    //     } catch (error) {
+    //         requestArray[index] = {state: 'error'}
+    //         console.error('Error posting file data from API:', error.response);
+    //     }
+    // };
+
+    const postPDF = (file, index) => {
+        return fetch(createURL(), 
+            {
+                method: 'POST',
                 headers: {
-                    Accept: 'application/json'
+                    'Content-Type': 'application/json' // Specify the content type in the headers
                 },
-                body: base64Content
+                body: JSON.stringify({
+                    'encoded_pdf': fileToBase64(file),
+                    'file_name': file.name
+                })    
             })
-            if (response.status == 200) {
-                requestArray[index] = {uuid: response.data.uuid, state: 'in progress'}
-            }
-        } catch (error) {
-            requestArray[index] = {state: 'error'}
-            console.error('Error posting file data from API:', error.response);
-        }
+            .then(response => {
+                if(!response.ok){
+                    requestArray[index] = {state: 'error'}
+                    throw new Error('bad status code');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("batch job was successfully submitted");
+                requestArray[index] = {uuid: data.uuid, state: 'in progress'}
+                return data.uuid
+            })
+            .catch(error => {
+                requestArray[index] = {state: 'error'}
+                console.error('upload pdf error:', error);
+                throw error;
+            });
     };
 
     // get information from posted files, returns if another call needs to be made
-    const getFileInformation = async (uuid, index) => {
-        try {
-            const response = await axios.get(createURL(uuid))
-            if (response.status == 200) { // completed
-                requestArray[index] = {state: 'completed'}
-                updateRequirementList(response.data.job_output, index)
-            }
-            else if (response.status == 202) { // in progress
-                return true
-            }
-        } catch (error) {
-            requestArray[index] = {state: 'error'}
-            console.error('Error getting file data from API:', error.response);
-        }
-        return false
-    };
+    // const getFileInformation = async (uuid, index) => {
+    //     try {
+    //         const response = await axios.get(createURL(uuid))
+    //         if (response.status == 200) { // completed
+    //             requestArray[index] = {state: 'completed'}
+    //             updateRequirementList(response.data.job_output, index)
+    //         }
+    //         else if (response.status == 202) { // in progress
+    //             return true
+    //         }
+    //     } catch (error) {
+    //         requestArray[index] = {state: 'error'}
+    //         console.error('Error getting file data from API:', error.response);
+    //     }
+    //     return false
+    // };
 
-    const checkFileRequests = async () => {
-        var notCompleted = false
-        await new Promise(r => setTimeout(r, 30000)); // wait 30 sec
-        requestArray.map((request, index) => {
-            if (request.state == 'in progress') {
-                if (getFileInformation(request.uuid, index)) {
-                    notCompleted = true
+    const getPDF = (uuid, index, retryDelay = 30000) => {
+        return fetch(createURL(uuid))
+            .then(response => {
+                if(response.status === 200){
+                    requestArray[index] = {state: 'completed'}
+                    updateRequirementList(response.data.job_output, index)
                 }
-            }
-
-        })
-        return notCompleted
+                else if(response.status === 202){
+                    return new Promise(resolve =>
+                        setTimeout(()=> resolve(getPDF(url, uuid, retryDelay)), retryDelay)
+                    );
+                }
+                else{
+                    requestArray[index] = {state: 'error'}
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+            })
+            .catch(error => {
+                requestArray[index] = {state: 'error'}
+                console.error('get pdf error:', error);
+                throw error;
+            }); 
     }
+
+    // const checkFileRequests = async () => {
+    //     var notCompleted = false
+    //     await new Promise(r => setTimeout(r, 30000)); // wait 30 sec
+    //     requestArray.map((request, index) => {
+    //         if (request.state == 'in progress') {
+    //             if (getFileInformation(request.uuid, index)) {
+    //                 notCompleted = true
+    //             }
+    //         }
+
+    //     })
+    //     return notCompleted
+    // }
 
     useEffect(() => {
         for (const index in files) {
-            postFile(files[index], index)
+            postPDF(files[index], index)
         }
 
-        var notcompleted = true
-        var count = 0
-        while (count < 20 && notcompleted) { // wait up to 20 min
-            notcompleted = checkFileRequests()
-            count += 1;
-        }
+        requestArray.map((request, index) => {
+            if (request.state == 'in progress') {
+                getPDF(request.uuid, index);
+            }
+        });
         // update to show time out error if needed
     }, [])
 
