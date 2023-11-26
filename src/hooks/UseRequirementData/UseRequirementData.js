32ed1838411d76fe52784dfb4515280be5c60ab7
rqualls <rachellqualls@gmail.com>
2023-11-25 22:33:13 -0600

    clean up code and fix some issues

--- src/hooks/UseRequirementData/UseRequirementData.js
+++ src/hooks/UseRequirementData/UseRequirementData.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react';
-// import InspectMocks from '../../views/Inspect/_test_/mocks';
 import axios from 'axios';
 import jsonData from '../../config.json';
 import PropTypes from 'prop-types';
 
 const UseRequirementData = (files) => {
-    // const [requirementsList, setrequirementsList] = useState(InspectMocks.requirementsList);
     const [requirementsList, setrequirementsList] = useState(files.map(() => null));
     const apiDomain = jsonData.apiURL
     var requestArray = files.map(() => {return {state: 'not started'}});
 
     // create the URL for the request
     const createURL = (uuid=null) => {
-        // return (uuid === null) ? `${apiDomain}upload/`: `${apiDomain}upload/${uuid}`
-        return (uuid === null) ? `https://j2fvtjt45i.execute-api.us-east-1.amazonaws.com/Prod/api/upload`: `https://j2fvtjt45i.execute-api.us-east-1.amazonaws.com/Prod/api/upload/${uuid}`
+        return (uuid === null) ? `${apiDomain}upload/`: `${apiDomain}upload/${uuid}`;
     }
 
     const updateRequirementList = (newRequirements, updateIndex) => {
@@ -67,37 +64,6 @@ const UseRequirementData = (files) => {
         }
     };
 
-    // const postPDF = (file) => {
-    //     return fetch(createURL(), 
-    //         {
-    //             mode: 'no-cors',
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json' // Specify the content type in the headers
-    //             },
-    //             body: JSON.stringify({
-    //                 'encoded_pdf': fileToBase64(file),
-    //                 'file_name': file.name
-    //             })    
-    //         })
-    //         .then(response => {
-    //             if(!response.ok){
-    //                 console.error(`HTTP error! Status: ${response.status}`);
-    //             }
-    //             return {status: 'error'};
-    //         })
-    //         .then(data => {
-    //             console.log("batch job was successfully submitted");
-    //             console.log(data);
-    //             console.log(data.uuid);
-    //             return {status: 'ok', uuid: data.uuid}
-    //         })
-    //         .catch(error => {
-    //             console.error('upload pdf error:', error);
-    //             return {status: 'error'};
-    //         });
-    // };
-
     // get information from posted files, returns if another call needs to be made
     const getFileInformation = async (uuid, index) => {
         try {
@@ -119,34 +85,8 @@ const UseRequirementData = (files) => {
         return false
     };
 
-    // const getPDF = (uuid, retryDelay=30000, maxRetry=20) => {
-    //     return fetch(createURL(uuid))
-    //         .then(response => {
-    //             if(response.status === 200){
-    //                 return {status: 'ok', response: response.json()};
-    //             }
-    //             else if(response.status === 202){
-    //                 if (maxRetry == 1) {
-    //                     return {status: 'timeout'};
-    //                 }
-    //                 return new Promise(resolve =>
-    //                     setTimeout(()=> resolve(getPDF(uuid, retryDelay, maxRetry-1)), retryDelay)
-    //                 );
-    //             }
-    //             else{
-    //                 console.error(`HTTP error! Status: ${response.status}`);
-    //                 return {status: 'error'};
-    //             }
-    //         })
-    //         .catch(error => {
-    //             console.error('get pdf error:', error);
-    //             return {status: 'error'};
-    //         }); 
-    // };
-
     const checkFileRequests = async () => {
         var completed = true;
-        // await new Promise(r => setTimeout(r, 30000)); // wait 30 sec
         requestArray.map((request, index) => {
             if (request.state == 'in progress') {
                 console.log(`checking request of ${request}`)
@@ -160,47 +100,6 @@ const UseRequirementData = (files) => {
         })
         return completed
     }
-
-    useEffect(() => {
-        // for (const index in files) {
-        //     const response = postPDF(files[index]);
-        //     if (response.status === 'error') {
-        //         requestArray[index] = {state: 'error'};
-        //     } else {
-        //         requestArray[index] = {uuid: response.uuid, state: 'in progress'}
-        //     }
-        // }
-
-        // requestArray.map((request, index) => {
-        //     if (request.state == 'in progress') {
-        //         const response = getPDF(request.uuid);
-        //         if (response.status === 'error') {
-        //             requestArray[index] = {state: 'error'};
-        //         } else if (response.status === 'timeout') {
-        //             requestArray[index] = {state: 'timeout'};
-        //         } else {
-        //             requestArray[index] = {state: 'completed'}
-        //             updateRequirementList(response.data.job_output, index);
-        //         }
-        //     }
-        // });
-        // console.log('this is run')
-
-        // for (const index in files) {
-        //     console.log(`begin sending info of file ${index}`)
-        //     postFile(files[index], index);
-        //     console.log(`done sending info of file ${index}`)
-        // }
-
-        // var notcompleted = true
-        // var count = 0
-        // while (count < 20 && notcompleted) { // wait up to 20 min
-        //     console.log(`checking count ${count}`)
-        //     notcompleted = checkFileRequests();
-        //     count += 1;
-        // }
-        // update to show time out error if needed
-    }, [])
 
     useEffect(() => {
         console.log('this is run')
--- src/hooks/UseRequirementData/_test_/UseRequirementData.test.js
+++ src/hooks/UseRequirementData/_test_/UseRequirementData.test.js
@@ -1,29 +1,29 @@
 import React from 'react'; 
-import { enableFetchMocks } from 'jest-fetch-mock';
+import axios from 'axios';
 import { renderHook, waitFor } from '@testing-library/react';
 import UseRequirementData from '../UseRequirementData';
 
-enableFetchMocks();
+jest.mock('axios');
 
 describe('UseRequirementData', () => {
 
   test('Should initially return a null array with the number of elements', () => {
     const { result } = renderHook(() => UseRequirementData([new File(['file1'], 'file1.pdf'), new File(['file2'], 'file2.pdf'), new File(['file3'], 'file3.pdf')]));
-    const { requirementsList } = result.current;
+    const requirementsList = result.current;
     expect(requirementsList[0]).toBe(null);
     expect(requirementsList[1]).toBe(null);
     expect(requirementsList[2]).toBe(null);
   });
 
   test('Should return file information', async () => {
-    fetch.mockResponseOnce(JSON.stringify({ uuid: '1234' }), { status: 200 });
-    fetch.mockResponseOnce(JSON.stringify({ uuid: '5678' }), { status: 200 });
-    fetch.mockResponseOnce(JSON.stringify({ uuid: '9012' }), { status: 200 });
-    fetch.mockResponseOnce(JSON.stringify({ body: {job_output: 'output1'} }), { status: 200 });
-    fetch.mockResponseOnce(JSON.stringify({ body: {job_output: 'output2'} }), { status: 200 });
-    fetch.mockResponseOnce(JSON.stringify({ body: {job_output: 'output3'} }), { status: 200 });
+    axios.post.mockResolvedValueOnce(JSON.stringify({ uuid: '1234' }), { status: 200 });
+    axios.post.mockResolvedValueOnce(JSON.stringify({ uuid: '5678' }), { status: 200 });
+    axios.post.mockResolvedValueOnce(JSON.stringify({ uuid: '9012' }), { status: 200 });
+    axios.get.mockResolvedValueOnce(JSON.stringify({ body: {job_output: 'output1'} }), { status: 200 });
+    axios.get.mockResolvedValueOnce(JSON.stringify({ body: {job_output: 'output2'} }), { status: 200 });
+    axios.get.mockResolvedValueOnce(JSON.stringify({ body: {job_output: 'output3'} }), { status: 200 });
     const { result } = renderHook(() => UseRequirementData([new File(['file1'], 'file1.pdf'), new File(['file2'], 'file2.pdf'), new File(['file3'], 'file3.pdf')]));
-    const { requirementsList } = result.current;
+    const requirementsList = result.current;
     // axios.post.mockResolvedValueOnce({status: 200, data: {uuid: '1234'}});
     // axios.post.mockResolvedValueOnce({status: 200, data:{uuid: '5678'}});
     // axios.post.mockResolvedValueOnce({status: 200, data:{uuid: '9123'}});
