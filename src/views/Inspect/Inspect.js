77921f2f21d90a0902b3404def983ac74b580879
rqualls <rachellqualls@gmail.com>
2023-11-28 17:26:31 -0600

    hate it here

--- src/views/Inspect/Inspect.js
+++ src/views/Inspect/Inspect.js
@@ -69,6 +69,12 @@ const Inspect = ({ setPage, uploadedFiles, setUploadedFiles, testingRequirements
   useEffect(() => {
     console.log(`requirementsList updated:`);
     console.log(requirementsList)
+    if (requirementsList[0] !== null) {
+      console.log(requirementsList[0]);
+      console.log(typeof requirementsList[0]);
+      console.log(requirementsList[0]['header']);
+      console.log(typeof requirementsList[0]['header']);
+    };
     // update met conditions
     const newConditions = metConditions.map((conditon, index) => {
       if (requirementsList[index] !== null && conditon === null) {
